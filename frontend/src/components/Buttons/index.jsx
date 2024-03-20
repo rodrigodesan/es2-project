1d3308e9c38a08e16ac72e38ba8d90b7328d025f
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-03-19 23:45:59 -0300

    feat(search-exec): applies search execution to the components

--- frontend/src/components/Buttons/index.jsx
+++ frontend/src/components/Buttons/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function Button({
-  title, icon, action, color,
+  title, icon, action, color, onClick,
 }) {
   const buttonClass = `btn ${color} rounded-5 mt-3 px-4 ms-4 `;
   return (
     <div>
-      <button type="submit" className={buttonClass}>
+      <button type="button" className={buttonClass} onClick={onClick}>
         <a className="text-decoration-none text-reset" href={action}>
           {title}
           {' '}
