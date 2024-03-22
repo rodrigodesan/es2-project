c6ddc0fc8b304163b3651b97f8bbec9035f79cc6
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-22 00:56:41 -0300

    feat: create page Team to test

---
+++ frontend/src/pages/Team/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+function Team() {
+  return (
+    <div>
+      <Navbar page="true" activePage="team" />
+      <h1 className="login-space">Team</h1>
+    </div>
+  );
+}
+
+export default Team;
