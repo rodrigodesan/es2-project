de1fac9dc36426223fb3ac4b7e41d8f40b372515
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-22 10:50:41 -0300

    feat(front): update all routes to respective paths

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Team from './pages/Team';
+import Sobre from './pages/Sobre';
+import Recursos from './pages/Recursos';
+import Perfil from './pages/Perfil';
+import BuscasSalvas from './pages/BuscasSalvas';
 import { DashProvider } from './contexts/dashContext';
 
 export function Router() {
@@ -11,7 +15,11 @@ export function Router() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/recursos" element={<Recursos />} />
         <Route path="/home" element={<DashProvider><Dashboard /></DashProvider>} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/buscas-salvas" element={<BuscasSalvas />} />
       </Routes>
     </BrowserRouter>
   );
