2350f5a70392cb172fecf269e46f62ba9173776a
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-22 00:43:21 -0300

    style(box): add margin top to fix navbar overlap

--- frontend/src/components/Box/index.jsx
+++ frontend/src/components/Box/index.jsx
@@ -33,7 +33,7 @@ function Box({
   }
 
   return (
-    <div className={`rounded-5 m-5 mb-0 p-4 border border-dark ${disabled && 'selection-bg'}`} style={{ cursor: disabled && 'not-allowed' }}>
+    <div className={`rounded-5 dashboard-space mx-5 mb-0 p-4 border border-dark ${disabled && 'selection-bg navbarspace'}`} style={{ cursor: disabled && 'not-allowed' }}>
       <h3 className="fw-bold fs-4 box-font">{title}</h3>
       <select disabled={disabled} className="w-100 selection-bg rounded-5 p-1 border border-dark text-dark" onChange={onChange}>
         <option className="text-center" value={0}>
