8764b2a70975b5f9de5463f85137a6fc60afb5db
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-22 10:52:09 -0300

    feat(navbar): update links, and add prop activePage

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function DashboardNavbar() {
+function DashboardNavbar({ activePage }) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbarColor fixed-top">
@@ -12,19 +13,19 @@ function DashboardNavbar() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="navbar-item px-3 ">
-                <a className="nav-link active fs-4 fw-bold" aria-current="page" href="#d">Dashboard</a>
+                <Link className={`nav-link fs-4 ${activePage === 'dashboard' ? 'active fw-bold' : ''}`} aria-current="page" to="/home">Dashboard</Link>
               </li>
               <li className="nav-item px-3">
-                <a className="nav-link fs-4" href="#d">Perfil</a>
+                <Link className={`nav-link fs-4 ${activePage === 'perfil' ? 'active fw-bold' : ''}`} to="/perfil">Perfil</Link>
               </li>
               <li className="nav-item px-3">
-                <a className="nav-link fs-4" href="#d">Busca Salvas</a>
+                <Link className={`nav-link fs-4 ${activePage === 'buscas-salvas' ? 'active fw-bold' : ''}`} to="/buscas-salvas">Busca Salvas</Link>
               </li>
               <li className="nav-item px-2">
-                <a className="nav-link fs-4" href="#d">Olá, Convidado</a>
+                <Link className="nav-link fs-4" to="/perfil">Olá, Convidado</Link>
               </li>
               <li className="nav-item px-1">
-                <button className="btn bt-navbar  rounded-5 px-3 fs-5 border border-dark" type="submit">Logout</button>
+                <Link to="/"><button className="btn bt-navbar  rounded-5 px-3 fs-5 border border-dark" type="submit">Logout</button></Link>
               </li>
             </ul>
           </div>
@@ -34,30 +35,31 @@ function DashboardNavbar() {
   );
 }
 
-function HomeNavbar() {
+function HomeNavbar({ activePage }) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbarColor fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand pt-0 pb-0 logo" href="/home"><img src="./src/assets/logo/logo.png" alt="s" /></a>
+          <Link className="navbar-brand pt-0 pb-0 logo" to="/"><img src="./src/assets/logo/logo.png" alt="s" /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              <li className="navbar-item px-3 ">
-                <a className="nav-link active fs-4 fw-bold" aria-current="page" href="#d">Sobre</a>
+              <li className="navbar-item px-3">
+                <Link className={`nav-link fs-4  ${activePage === 'sobre' ? 'active fw-bold' : ''}`} to="/sobre">Sobre</Link>
               </li>
-              <li className="nav-item px-3">
-                <a className="nav-link fs-4" href="#d">Contato</a>
+              <li className="navbar-item px-3">
+                <Link className={`nav-link fs-4  ${activePage === 'team' ? 'active fw-bold' : ''}`} to="/team">Team</Link>
               </li>
-              <li className="nav-item px-3">
-                <a className="nav-link fs-4" href="#d">Recursos</a>
+              <li className="navbar-item px-3">
+                <Link className={`nav-link fs-4 ${activePage === 'recursos' ? 'active fw-bold' : ''}`} to="/recursos">Recursos</Link>
               </li>
-              <li className="nav-item px-1">
-                <button className="btn bt-navbar rounded-5 px-3 fs-5 border border-dark" type="submit">Entrar como convidado</button>
+              <li className="navbar-item px-1">
+                <Link to="/home">
+                  <button className="btn bt-navbar rounded-5 px-3 fs-5 border border-dark" type="submit">Entrar como convidado</button>
+                </Link>
               </li>
-
             </ul>
           </div>
         </div>
@@ -66,8 +68,8 @@ function HomeNavbar() {
   );
 }
 
-function Navbar({ page }) {
-  return page === 'true' ? <HomeNavbar /> : <DashboardNavbar />;
+function Navbar({ page, activePage }) {
+  return page === 'true' ? <HomeNavbar activePage={activePage} /> : <DashboardNavbar activePage={activePage} />;
 }
 
 export default Navbar;
