5916db79afe790e298c52f749f3f48478ffdc3d3
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-23 12:55:47 -0300

    refactor(dashboard): changes the createOptions methos to follow factory method pattern

--- frontend/src/pages/Dashboard/utils.js
+++ frontend/src/pages/Dashboard/utils.js
@@ -1,4 +1,21 @@
-export function createOptions(result) {
+// Design Pattern: Factory Method
+// Interface
+class OptionsFactory {
+  // eslint-disable-next-line no-unused-vars
+  createOption(id, label) {
+    throw new Error('O método createOption deve ser implementado por subclasses');
+  }
+}
+
+// Implementação concreta do Factory Method
+class DefaultOptionsFactory extends OptionsFactory {
+  createOption(id, label) {
+    return { value: id, label: String(label) };
+  }
+}
+
+// Classe de cliente
+export function createOptions(result, factory = new DefaultOptionsFactory()) {
   const options = [{ value: 0, label: 'Todos(as)' }];
 
   result?.forEach((element) => {
@@ -6,7 +23,7 @@ export function createOptions(result) {
       id, year, name, item,
     } = element;
 
-    options.push({ value: id, label: (year && String(year)) || name || item });
+    options.push(factory.createOption(id, year || name || item));
   });
 
   return options;
