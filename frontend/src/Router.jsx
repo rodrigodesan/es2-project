9bea69837376398889b8c25e1dc0ba05cbeedf72
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-03-23 19:22:19 -0300

    feat(login-exec): creates the auth context

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -8,19 +8,22 @@ import Recursos from './pages/Recursos';
 import Perfil from './pages/Perfil';
 import BuscasSalvas from './pages/BuscasSalvas';
 import { DashProvider } from './contexts/dashContext';
+import { AuthProvider } from './contexts/authContext';
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/recursos" element={<Recursos />} />
-        <Route path="/home" element={<DashProvider><Dashboard /></DashProvider>} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/buscas-salvas" element={<BuscasSalvas />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/recursos" element={<Recursos />} />
+          <Route path="/home" element={<DashProvider><Dashboard /></DashProvider>} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/buscas-salvas" element={<BuscasSalvas />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
