371f3b30a1baba69aa21b1946b885767a3a7228b
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-24 02:53:30 -0300

    feat(profile_ui): update page BuscasSalvas

--- frontend/src/pages/BuscasSalvas/index.jsx
+++ frontend/src/pages/BuscasSalvas/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import SaveSearchs from '../../components/SaveSearchs';
 
 function BuscasSalvas() {
   return (
     <div>
       <Navbar page="false" activePage="buscas-salvas" />
-      <h1 className="mt-5 dashboard-space text-center font1">Buscas Salvas Page</h1>
+
+      <div className="dashboard-space">
+        <h1 className="font2 mt-5 ms-1 ps-4 fs-4 fw-bolder">Buscas Salvas 📌</h1>
+        <SaveSearchs title="Processos por estados" />
+        <SaveSearchs title="Média de duração" />
+        <SaveSearchs title="Média de duração" />
+        <SaveSearchs title="Média de duração" />
+      </div>
     </div>
   );
 }
