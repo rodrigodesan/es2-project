e6963334ec6caa116885dd1d0f75dd71c8a1fe5c
Marcos Paulo <marcos.paullo32@gmail.com>
2024-03-25 15:42:37 -0300

    fix(frontend_login_ui) : adjust margin of inputs and add a new class to signUp

--- frontend/src/components/SignIn/index.jsx
+++ frontend/src/components/SignIn/index.jsx
@@ -29,12 +29,12 @@ function SignIn() {
       ) : <div className="mt-5 mb-3" />}
       <div className="ms-1">
         <span className="m-2 signUp-font" id="basic-addon1">Login:</span>
-        <input type="text" className="selection-bg text-center rounded-4 border border-dark mt-5 py-1 me-4" placeholder="email@example.com" aria-label="Login" aria-describedby="basic-addon1" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" className="selection-bg text-center rounded-4 border border-dark mt-5 py-1 me-5" placeholder="email@example.com" aria-label="Login" aria-describedby="basic-addon1" value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <div className="mt-5">
         <span className="m-2 signUp-font" id="basic-addon2">Senha:</span>
         <div className="input-with-icon">
-          <input type={senhaVisivel ? 'text' : 'password'} className="text-center selection-bg rounded-4 border border-dark py-1 me-4" placeholder="********" aria-label="Recipient's username" aria-describedby="basic-addon2" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type={senhaVisivel ? 'text' : 'password'} className="text-center selection-bg rounded-4 border border-dark py-1 me-5" placeholder="********" aria-label="Recipient's username" aria-describedby="basic-addon2" value={password} onChange={(e) => setPassword(e.target.value)} />
           <img className="eye-icon" role="presentation" src={senhaVisivel ? './src/assets/icons/eye-solid.svg' : './src/assets/icons/eye-slash-solid.svg'} alt="Mostrar senha" onClick={alternarVisibilidadeSenha} />
         </div>
       </div>
