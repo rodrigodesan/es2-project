9eed768ecd5f6a9b3f8777042fcc530153c57c39
rodrigodesan <hiprodrigo@gmail.com>
2024-08-07 23:05:03 -0300

    change it back to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import app from './app';
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 const port = process.env.APP_PORT;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+export default app;
